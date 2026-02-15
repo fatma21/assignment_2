fdabc2dcc4ab0758a861320e62ae7d9a1400f2e0
Fatma <fatmaelzemiti@gmail.com>
2026-02-15 19:56:11 +0200

    Finished homescreen with singchildscroll view

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -24,109 +24,144 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Container(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image.asset(
-                  "assets/images/Untitled design (1).png",
-                  width: 140,
-                  height: 140,
-                ),
-                Expanded(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                height: 700,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.asset(
+                      "assets/images/Untitled design (1).png",
+                      width: 140,
+                      height: 140,
+                    ),
+                    Expanded(
 
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          cursorColor: Colors.blue,
-                          decoration: InputDecoration(
-                            hintText: 'Mobile number or Email address',
-                            hintStyle: const TextStyle(
-                              color: Color(0xFF898F9C),
-                              fontSize: 16,
-                            ),
-                            fillColor: Colors.white,
-                            contentPadding: const EdgeInsets.symmetric(
-                              vertical: 16,
-                              horizontal: 16,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide: const BorderSide(
-                                color: Color(0xFF898F9C),
-                                width: 2,
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              autovalidateMode: AutovalidateMode.onUserInteraction,
+                              cursorColor: Colors.blue,
+                              decoration: InputDecoration(
+                                hintText: 'Mobile number or Email address',
+                                hintStyle: const TextStyle(
+                                  color: Color(0xFF898F9C),
+                                  fontSize: 16,
+                                ),
+                                fillColor: Colors.white,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 16,
+                                  horizontal: 16,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                  borderSide: const BorderSide(
+                                    color: Color(0xFF898F9C),
+                                    width: 2,
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          validator: (value) {
-                            if (value == null || value.trim().isEmpty) {
-                              return 'Email is required';
-                            }
-                            if (!emailRegex.hasMatch(value.trim())) {
-                              return 'Enter a valid email address';
-                            }
-                            return null;
-                          },
-                        ),
-
-                        const SizedBox(height: 30),
-
-                        TextFormField(
-                          controller: passwordController,
-                          keyboardType: TextInputType.visiblePassword,
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          obscureText: show,
-                          decoration: InputDecoration(
-                            hintText: 'Enter your password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                show ? Icons.visibility : Icons.visibility_off,
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  show = !show;
-                                });
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return 'Email is required';
+                                }
+                                if (!emailRegex.hasMatch(value.trim())) {
+                                  return 'Enter a valid email address';
+                                }
+                                return null;
                               },
                             ),
-                          ),
-                          validator: (value) {
-                            if (value == null || value.trim().isEmpty) {
-                              return 'Password is required';
-                            }
 
-                            if (!passwordRegex.hasMatch(value.trim())) {
-                              return 'Password must contain:'
-                                  '- Uppercase'
-                                  '- Lowercase'
-                                  '- Number\n'
-                                  '- Special character'
-                                  '- Minimum 8 characters';
-                            }
-                            return null;
-                          },
+                            const SizedBox(height: 30),
+
+                            TextFormField(
+                              controller: passwordController,
+                              keyboardType: TextInputType.visiblePassword,
+                              autovalidateMode: AutovalidateMode.onUserInteraction,
+                              obscureText: show,
+                              decoration: InputDecoration(
+                                hintText: 'Enter your password',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    show ? Icons.visibility : Icons.visibility_off,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      show = !show;
+                                    });
+                                  },
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return 'Password is required';
+                                }
+
+                                if (!passwordRegex.hasMatch(value.trim())) {
+                                  return 'Password must contain:'
+                                      '- Uppercase'
+                                      '- Lowercase'
+                                      '- Number\n'
+                                      '- Special character'
+                                      '- Minimum 8 characters';
+                                }
+                                return null;
+                              },
+                            ),
+
+                            const SizedBox(height: 30),
+
+                            ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  Navigator.of(
+                                    context,
+                                  ).pushReplacementNamed("home");
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF1877F2),
+                                foregroundColor: Colors.white,
+                                minimumSize: const Size(double.infinity, 50),
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 16,
+                                  horizontal: 32,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                elevation: 5,
+                              ),
+                              child: const Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
+                      ),
+                    ),
 
-                        const SizedBox(height: 30),
-
+                    //Spacer(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
                         ElevatedButton(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              Navigator.of(
-                                context,
-                              ).pushReplacementNamed("home");
-                            }
-                          },
+                          onPressed: () {},
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF1877F2),
-                            foregroundColor: Colors.white,
-                            minimumSize: const Size(double.infinity, 50),
+                            backgroundColor: Colors.white,
+                            foregroundColor: Color(0xFF1877F2),
+                            side: BorderSide(color: Color(0xFF1877F2)),
                             padding: const EdgeInsets.symmetric(
                               vertical: 16,
                               horizontal: 32,
@@ -134,53 +169,23 @@ class _LoginScreenState extends State<LoginScreen> {
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(16),
                             ),
-                            elevation: 5,
                           ),
-                          child: const Text(
-                            "Login",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400,
-                            ),
+                          child: Text(
+                            "Create Account",
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
                           ),
+                        ),
+                        Image.asset(
+                          "assets/images/Untitled design (2).png",
+                          width: 80,
+                          height: 80,
                         ),
                       ],
                     ),
-                  ),
+
+                  ],
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Forgotten Password?",
-                    style: TextStyle(color: Color(0xFF898F9C)),
-                  ),
-                ),
-                //Spacer(),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Color(0xFF1877F2),
-                    side: BorderSide(color: Color(0xFF1877F2)),
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 16,
-                      horizontal: 32,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
-                  child: Text(
-                    "Create Account",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                  ),
-                ),
-                Image.asset(
-                  "assets/images/Untitled design (2).png",
-                  width: 80,
-                  height: 80,
-                ),
-              ],
+              ),
             ),
           ),
         ),
